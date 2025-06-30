94c14d924af876fe95f90ba55094001908432dc1
Felix Montanari <captainboggle108@gmail.com>
2025-06-30 16:49:13 +1000

    fix: added compatibility layer to ensure TensorFlow works on arm

--- tools/phenotagger/app.py
+++ tools/phenotagger/app.py
@@ -1,5 +1,7 @@
-from tool import PhenoTaggerModelImplementation
+# Import compatibility layer first to ensure TensorFlow is properly loaded
+import compat
 
+from tool import PhenoTaggerModelImplementation
 from goldmine.toolkit.api import create_app
 
 model_implementation = PhenoTaggerModelImplementation()
